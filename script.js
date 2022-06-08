0066225d244da2db1cae9b59331a0283d7094858
sietse333 <43068118+sietse333@users.noreply.github.com>
2022-06-08 21:31:48 +0200

    animatie

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let slideIndex = 0;
+
 showSlides();
 
 function showSlides() {
@@ -8,7 +9,28 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 4000);
 }
+
+const button = document.getElementById("mijKnop")
+const poppetje = document.getElementById("poppetje")
+const arm = document.getElementById("arm")
+var peek = new Audio('./mp3/peek.mp3');
+var woosh = new Audio('./mp3/wooshes.mp3');
+peek.volume = 0.4
+woosh.volume = 0.3
+
+button.addEventListener("click", function () {
+  peek.play();
+  setTimeout(function () {
+    woosh.play();
+  }, 1000)
+  poppetje.classList.add("poppetje-animatie");
+  arm.classList.add("arm-animatie");
+  button.style.display = "none";
+});
+
